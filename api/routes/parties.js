b1c558db7b1f25dfca1a9dccea23b4f0c3065696
Tim Willis <twillis@whitepages.com>
2019-07-30 14:48:17 -0700

    Pushing up changes

--- api/routes/parties.js
+++ api/routes/parties.js
@@ -1,18 +1,6 @@
 const router = require('express').Router()
 const Party = require('../models/party')
-
-const isValid = (req, res, next) => {
-    try {
-      const token = req.headers.authorization.split('Bearer ')[1]
-      const payload = jsonwebtoken.verify(token, SECRET_KEY)
-    } catch (e) {
-      console.error(e)
-      const error = new Error(`you haven't given me a token!`)
-      error.status = 401
-      return next(error)
-    }
-    next()
-}
+const validateToken = require('../helpers/validate')
 
 
 router.get('/', async (req, res, next) => {
@@ -22,7 +10,7 @@ router.get('/', async (req, res, next) => {
   res.json({ status, response })
 })
 
-router.get('/exclusive', isValid, async (req, res, next) => {
+router.get('/exclusive', validateToken, async (req, res, next) => {
   const status = 200
   
   const response = await Party.findOne({ exclusive: true }).select('-__v')
